9532162883bfa57d7af3731089d6b73c210062aa
akrem0-jr <abdulbasetadem@gmail.com>
2024-05-26 12:21:44 +0300

    admin auth

--- Exam_overflow.sql
+++ Exam_overflow.sql
@@ -1,16 +1,10 @@
 --
 -- Database: `Exam_overflow`
 --
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
-
-
-
 USE exam_overflow;
-
 CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -20,4 +14,13 @@ CREATE TABLE IF NOT EXISTS users (
     grade VARCHAR(50) NOT NULL,
     stream VARCHAR(50) NOT NULL,
     image VARCHAR(255) NOT NULL
+);
+-- Table structure for table `admin`
+CREATE TABLE IF NOT EXISTS `admin` (
+    `id` VARCHAR(50) PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    `profession` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL,
+    `image` VARCHAR(255) NOT NULL
 );
